bb5988e95010c04a0a44ed90992f55b3216ca475
printlnHi <printlnHi@users.noreply.github.com>
2017-08-01 10:13:53 +0800

    Made project landscape iPad only

--- ChatCCGS/ChatCCGS/ViewController.swift
+++ ChatCCGS/ChatCCGS/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Ignore this change
         // Do any additional setup after loading the view, typically from a nib.
     }
 

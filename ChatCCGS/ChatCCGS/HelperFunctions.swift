a4c1a387ad3a4cb7ba56fcfecc59023ea9ebba18
printlnHi <printlnHi@users.noreply.github.com>
2017-08-25 10:39:32 +0800

    updated query helper functions

--- ChatCCGS/ChatCCGS/HelperFunctions.swift
+++ ChatCCGS/ChatCCGS/HelperFunctions.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
-func escapeStringForQuery(queryString s: String) -> String{
-    return s.replacingOccurrences(of: " ", with: "%20")
+class QueryHelper{
+    static func escapeStringForQuery(queryString s: String) -> String{
+        return s.replacingOccurrences(of: " ", with: "%20")
+    }
+    
+    static let tartarusUsername = "ccgs"
+    static let tartarusPassword = "1910"
+    static var userUsername = "USERNAME NOT SET"
+    static var userPassword = "PASSWORD NOT SET"
 }
+

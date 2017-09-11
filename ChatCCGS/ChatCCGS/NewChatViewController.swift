c0ada9cbaf3e9c20953caa142793859540b357a2
Nick Patrikeos <1019912@student.ccgs.wa.edu.au>
2017-09-11 18:58:49 +0800

    new chats can be added

--- ChatCCGS/ChatCCGS/NewChatViewController.swift
+++ ChatCCGS/ChatCCGS/NewChatViewController.swift
@@ -15,11 +15,13 @@ class NewChatViewController: ViewController, UITableViewDelegate, UITableViewDat
     var currentStudent: Student = Student()
     var selectedPeople = [Student]()
     
+    @IBOutlet weak var groupName: UITextField!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("A new chat")
         print(currentStudent)
+        selectedPeople.append(currentStudent)
         // Do any additional setup after loading the view.
     }
 
@@ -29,13 +31,50 @@ class NewChatViewController: ViewController, UITableViewDelegate, UITableViewDat
     }
 
     @IBAction func createNewGroupChat() {
-        var groupChat = CustomGroupChat()
         
-        
+        if selectedPeople.count <= 1 {
+            alertNotValid()
+        } else {
+            var groupChat = CustomGroupChat()
+            groupChat.name = groupName.text!
+            
+            for i in selectedPeople {
+                groupChat.members.append(i)
+            }
+            
+            print(groupChat)
+            
+            var request = "http://tartarus.ccgs.wa.edu.au/~1022309/cgibin/ChatCCGS/CustomGroups/createGroup.py?username="
+            request += currentStudent.ID + "&password="
+            request += "password123" + "&name="
+            request += groupChat.name.replacingOccurrences(of: " ", with: "%20") + "&members="
+            
+            var members = "["
+            var count = groupChat.members.count
+            for i in groupChat.members {
+                if count != 1 {
+                    members += i.ID + ","
+                } else {
+                    members += i.ID
+                }
+                count -= 1
+            }
+            members += "]"
+            
+            request += members
+            
+            print(members)
+            print(request)
+            
+            Alamofire.request(request).authenticate(user: "ccgs", password: "1910").responseString { response in
+                debugPrint(response.result.value!)
+            }
+        }
     }
     
-    /*func getIndividualContacts() -> [Student] {
-    }*/
+    func alertNotValid() {
+        
+    }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return getRecentChats().count
@@ -45,22 +84,31 @@ class NewChatViewController: ViewController, UITableViewDelegate, UITableViewDat
         print("HELLO!")
         
         let cell = tableView.cellForRow(at: indexPath)
+        print(indexPath)
+        print(cell?.contentView.backgroundColor)
+        print(cell?.tag)
         
-        if cell?.contentView.backgroundColor != UIColor.green {
+        if (cell?.tag)! == 0 {
             selectedPeople.append(getRecentChats()[indexPath.row].person1!)
             print(selectedPeople)
-            cell?.contentView.backgroundColor = UIColor.green
+            let chat = getRecentChats()[indexPath.row]
+            
+            cell?.textLabel?.text = (chat.person1?.name)! + " [SELECTED]"
+            cell?.tag = 1
         } else {
             selectedPeople.remove(at: getIndexFromSelected(of: (cell?.textLabel?.text!)!)!)
             print(selectedPeople)
-            cell?.contentView.backgroundColor = UIColor.white
+            let chat = getRecentChats()[indexPath.row]
+            cell?.textLabel?.text = (chat.person1?.name)!
+            cell?.tag = 0
+            
         }
     }
     
     func getIndexFromSelected(of student: String) -> Int? {
         var x = 0
         for s in selectedPeople {
-            if student == s.name {
+            if student + " [SELECTED]" == s.name {
                 return x
             }
             x += 1
